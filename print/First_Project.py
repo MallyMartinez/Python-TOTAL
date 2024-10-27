143439dee051386f4f74e760abb502cd998782c3
MallyMartinez <mallymartinez20@gmail.com>
2024-10-27 06:01:30 -0600

    exercises

--- print/First_Project.py
+++
@@ -1,8 +0,0 @@
-#mostrar texto
-print ("Hola mundo")
-
-#mostrar numero
-print("25")
-
-#mostrar operacion
-print(158+42)
---
+++ print/theory.py
@@ -0,0 +1,27 @@
+"""
+P R I N T
+    Declaración que al ejecutarse muestra (o imprime) en pantalla 
+    el argumento que se introduce dentro de los paréntesis.
+"""
+
+""" 
+    Mostrar texto
+        Ingresamos entre comillas simples o dobles los 
+        caracteres detexto que deben mostrarse en pantalla.
+"""
+print ("Hello, world!")
+
+
+""" 
+    Mostrar numeros
+        Podemos entregarle a print() el número que debe mostrar,
+        o una operación matemática a resolver. No empleamos 
+        comillas en estos casos.
+"""
+print("25")
+
+print(158+42)
+
+
+# Combinados 
+print("The result is:", 42)
