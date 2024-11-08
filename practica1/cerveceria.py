953e50a53e8d7395e6fecd9b9204f635b98bddd1
MallyMartinez <mallymartinez20@gmail.com>
2024-11-07 23:57:13 -0600

    1st practice
    case: brewery name

---
+++ practica1/cerveceria.py
@@ -0,0 +1,26 @@
+"""
+Practica 1: cerveceria
+    Tu mejor amigo ha puesto una fábrica de cerveza y tiene todo listo.
+    Su producto es fantástico, tiene cuerpo, buen sabor, buen color, 
+    el nivel justo de espuma.Pero le falta una identidad.
+
+    No se le ocurre qué nombre ponerle su cerveza para que tenga 
+    una identidad única y original.
+
+*   Entonces, vas a crear un programa que te va a hacer dos preguntas 
+*   y luego te va a decir cuál es el nombre de tu cerveza, 
+*   para formar una marca creativa.
+
+    Así de simple. Ya sé que en el mundo real no necesitaríamos desarrollar 
+    un software solo para hacer dos preguntas,pero hasta que aprendamos más 
+    funcionalidades, bueno, los programas van a tener que mantenerse en
+    el terreno de lo simple.
+"""
+uno = input("Escribe lo primero que se te venga a la mente: ")
+dos = input("Escribe lo segundo que se te venga a la mente: ")
+print('Tu cerveza se llama \n"' + uno + ' ' + dos + '"')
+
+
+#? Ejemplo del curso 
+print("El nombre de tu cerveza\nes '" + input("Que ciudad te gustaria visitar?: ") 
+      + " " + input("Cual es tu color favorito?: ") + "'\nFelicitaciones!")
