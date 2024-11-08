5fcf5be4bcddb55ad800d78d0353a99ae665cfa5
MallyMartinez <mallymartinez20@gmail.com>
2024-11-07 22:11:18 -0600

    File Theory
    File created and performed

---
+++ strings/theory.py
@@ -0,0 +1,38 @@
+"""
+S T R I N G S
+    Los strings en Python son un tipo de dato formado por
+    cadenas (o secuencias) de caracteres de cualquier tipo,
+    formando un texto.
+"""
+
+#Se utiliza comillas simples o dobles para definir un string
+print('100+50')
+print("Bienvenido")
+
+"""
+Concatenacion
+    Es la union de dos o mas cadenas de texto
+"""
+print("Hola" + " " + "mundo")
+print("Como " + 'esta?')
+
+"""
+Caracteres esoeciales
+    Indicamos a la consola que el caracter a continuación del
+    símbolo \ debe ser tratado como un caracter especial.
+
+"""
+#\" > Imprime comillas
+print("Me llamo \"Mally\"")
+
+#\n > Separa texto en una nueva linea
+print('Esta es una linea\nY esta es otra linea')
+
+#\t > Imprime un tabulador
+print("\tEsta es la tercera linea")
+
+#\' > Imprime el apostrofe
+print('This isn\'t a number')
+
+#\\ > Imprime la barra invertida textualmente
+print('Este signo \\ es una barra invertida')
