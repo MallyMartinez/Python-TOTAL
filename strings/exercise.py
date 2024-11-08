3ca4e9eeae7fe23910ebf76c504d3ee385a70e82
MallyMartinez <mallymartinez20@gmail.com>
2024-11-07 22:23:08 -0600

    exercises
    String manipulation exercises

---
+++ strings/exercise.py
@@ -0,0 +1,26 @@
+"""
+Pratica String 1
+    Crea un código que imprima en pantalla la siguiente 
+    expresión (pero usando print una sola vez):
+    1| Línea 1
+    2| Línea 2
+    3| Línea 3
+"""
+print("Linea 1"+"\nLinea 2"+"\nLinea 3")
+
+"""
+Pratica String 2
+    Crea un código que imprima en pantalla la siguiente expresión:
+    1| A	B	C
+    2| D	E	F
+    3| G	H	I
+"""
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+
+"""
+Pratica String 3
+    Crea un código que imprima en pantalla la siguiente expresión:
+    1| Barra Normal: /
+    2| Barra Invertida: \
+"""
+print("Barra Normal: /\n"+"Barra Invertida: \\")
