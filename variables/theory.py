23e7f1da6dae8f83452fbe00af41e20ec21eec1a
MallyMartinez <mallymartinez20@gmail.com>
2024-11-08 04:30:59 -0600

    Theory: variables
    File created and performed with variables

---
+++ variables/theory.py
@@ -0,0 +1,35 @@
+"""
+V A R I A B L E S
+    Las variables son espacios de memoria que almacenan valores
+    o datos de distintos tipos, y (como su nombre indica) pueden
+    variar. Se crean en el momento que se les asigna un valor, por
+    lo cual en Python no requerimos declararlas previamente.
+"""
+#primer valor
+nombre = "Mally"
+print(nombre)
+
+#nuevo valor qe reemplaza el primero
+nombre = "Samira"
+print(nombre)
+
+#tipo numerico
+edad1 = 25
+edad2 =  41
+print(edad1+ edad2)
+
+#con input
+ciudad = input("dime tu ciudad: ")
+print("tu ciudad es: ",ciudad)
+
+#concatenar
+color1 = "azul"
+color2 = "rojo"
+frase = color1 +" y "+ color2
+print(frase)
+
+#concatenar con numeros
+num1 = 50
+num2 = 30
+frase = num1 + num2
+print(frase)
